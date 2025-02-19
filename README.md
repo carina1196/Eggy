66c605de26c92f3a08145cad04b10c07a20ed5ad
carina1196 <carinachanqingwei@gmail.com>
2025-02-19 23:21:45 +0800

    Egg timer with the updated UI and logic

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Eggy
+# Eggy -  Egg Timer
+
+A timer created to help users cook their egg to their preferred choice.
+
+Simply select an egg of your choice and start the timer.
+
+Please note that this timer is for fridge cold eggs!
+
+Boil your water first and lower the eggs into the boiling water and start your timer!
---
+++ app.js
@@ -0,0 +1,68 @@
+const eggTiming = {"runny": 300, "soft": 480, "hard":600}
+
+function setTimer(img) {
+    let duration;
+    const timerElement = document.getElementById("timer");
+    const x = img.id;
+
+    if (timerElement.innerText != '00:00'){
+        clearInterval(countdown)
+    }
+    duration = eggTiming[x]
+    const minutes = Math.floor(duration/60)
+    
+    timerElement.textContent = `${ minutes < 10 ? "0" : ""}${minutes}:00`;
+    document.getElementById('startBtn').disabled = false;
+    document.getElementById('stopBtn').disabled = true;
+}
+
+function updateTimer(durationLeft) {
+    const timerElement = document.getElementById("timer");
+    let minutes = Math.floor(durationLeft/60)
+    let seconds = durationLeft%60
+    timerElement.textContent = `${ minutes < 10 ? "0" : ""}${minutes}:${ seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+function startTimer() {
+    const time = document.getElementById("timer").innerText;
+    document.getElementById('startBtn').disabled = true;
+    document.getElementById('stopBtn').disabled = false;
+    let timeLeft;
+
+    switch (time){
+        case "05:00":
+            timeLeft = 300;
+            break
+        case "08:00":
+            timeLeft = 480;
+            break
+        case "10:00":
+            timeLeft = 600;
+            break
+        default:
+            timeLeft = 0
+    }
+    
+    countdown = 
+        setInterval(() => {
+            if (timeLeft == 0) {
+                document.getElementById("timer").textContent = '00:00'
+            } else {
+                timeLeft--;
+                updateTimer(timeLeft)
+            }
+        },1000)
+    
+}
+
+function stopTimer () {
+    const btn = document.getElementById('stopBtn');
+    if (btn.textContent == 'Stop'){
+        clearInterval(countdown);
+        btn.innerText = 'Reset'
+    } else if (btn.textContent == 'Reset') {
+        document.getElementById("timer").textContent = '00:00';
+        document.getElementById('stopBtn').disabled = true;
+        btn.innerText = 'Stop'
+    }
+}
